efb55e5c6624fcf196a1f84aa1ad4f46b14200a3
Kaseal <s.a.kashin@yandex.ru>
2015-05-26 12:11:47 +0300

    customtelcoaccelerator extension added

---
+++ customtelcoaccelerator/customtelcoacceleratorcore/gensrc/com/epam/training/core/constants/GeneratedCustomtelcoacceleratorCoreConstants.java
@@ -0,0 +1,57 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at May 26, 2015 10:49:41 AM                    ---
+ * ----------------------------------------------------------------
+ */
+package com.epam.training.core.constants;
+
+/**
+ * @deprecated use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedCustomtelcoacceleratorCoreConstants
+{
+	public static final String EXTENSIONNAME = "customtelcoacceleratorcore";
+	public static class TC
+	{
+		public static final String ACCESSORY = "Accessory".intern();
+		public static final String CLASSIFICATIONNOTBLANKCONSTRAINT = "ClassificationNotBlankConstraint".intern();
+		public static final String DEVICE = "Device".intern();
+		public static final String PRODUCTREFERENCESANDCLASSIFICATIONSCOMPONENT = "ProductReferencesAndClassificationsComponent".intern();
+		public static final String PRODUCTREFERENCESANDCLASSIFICATIONSFORDEVICESCOMPONENT = "ProductReferencesAndClassificationsForDevicesComponent".intern();
+		public static final String SERVICEADDON = "ServiceAddOn".intern();
+		public static final String SERVICEPLAN = "ServicePlan".intern();
+	}
+	public static class Attributes
+	{
+		public static class ClassAttributeAssignment
+		{
+			public static final String PRODUCTREFERENCESANDCLASSIFICATIONSCOMPONENTS = "productReferencesAndClassificationsComponents".intern();
+		}
+		public static class ComposedType
+		{
+			public static final String PRODUCTREFERENCESANDCLASSIFICATIONSCOMPONENTS = "productReferencesAndClassificationsComponents".intern();
+		}
+	}
+	public static class Enumerations
+	{
+		public static class SiteChannel
+		{
+			public static final String TELCO = "Telco".intern();
+		}
+	}
+	public static class Relations
+	{
+		public static final String PRODUCTACCESSORYCOMPONENT2CLASSATTRIBUTEASSIGNREL = "ProductAccessoryComponent2ClassAttributeAssignRel".intern();
+		public static final String PRODUCTREFERENCESANDCLASSIFICATIONSCOMPONENT2TARGETITEMTYPEREL = "ProductReferencesAndClassificationsComponent2TargetItemTypeRel".intern();
+	}
+	
+	protected GeneratedCustomtelcoacceleratorCoreConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
---
+++ customtelcoaccelerator/customtelcoacceleratorfacades/gensrc/com/epam/training/facades/constants/GeneratedYtelcoacceleratorfacadesConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at May 26, 2015 10:49:41 AM                    ---
+ * ----------------------------------------------------------------
+ */
+package com.epam.training.facades.constants;
+
+/**
+ * @deprecated use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedYtelcoacceleratorfacadesConstants
+{
+	public static final String EXTENSIONNAME = "customtelcoacceleratorfacades";
+	
+	protected GeneratedYtelcoacceleratorfacadesConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
