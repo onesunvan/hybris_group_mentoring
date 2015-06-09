071d2c62b1c628739df1bffeb1e0dadf01171b25
Mykola Bilyi <mykola_bilyi@epam.com>
2015-06-09 15:00:12 +0300

    Story 2. Generate custom telco extensions.

---
+++ customtelcoacceleratorcockpits/gensrc/com/epam/training/cockpits/constants/GeneratedCustomtelcoacceleratorCockpitsConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Jun 9, 2015 2:37:42 PM                      ---
+ * ----------------------------------------------------------------
+ */
+package com.epam.training.cockpits.constants;
+
+/**
+ * @deprecated use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedCustomtelcoacceleratorCockpitsConstants
+{
+	public static final String EXTENSIONNAME = "customtelcoacceleratorcockpits";
+	
+	protected GeneratedCustomtelcoacceleratorCockpitsConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
---
+++ customtelcoacceleratorfacades/gensrc/com/epam/training/facades/constants/GeneratedYtelcoacceleratorfacadesConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Jun 9, 2015 2:37:42 PM                      ---
+ * ----------------------------------------------------------------
+ */
+package com.epam.training.facades.constants;
+
+/**
+ * @deprecated use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedYtelcoacceleratorfacadesConstants
+{
+	public static final String EXTENSIONNAME = "customtelcoacceleratorfacades";
+	
+	protected GeneratedYtelcoacceleratorfacadesConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
